2d0f92dc9150de62a843c2ad364bc139ca636701
Manick Mehra <manickmehra2006@yahoo.com>
2017-05-04 11:43:26 +0530

    Update reg.R

--- reg.R
+++ reg.R
@@ -1,5 +1,5 @@
 eruption.lm = lm(eruptions ~ waiting, data=faithful)
-
+aov(eruption.lm)
 newdata = data.frame(waiting=80)
 
 predict(eruption.lm, newdata) 
@@ -21,3 +21,15 @@ As the p-values of Air.Flow and Water.Temp are less than 0.05, they are both sta
 
 MLR
 As the p-values of the hp and wt variables are both less than 0.05, neither hp or wt is insignificant in the logistic regression model.
+
+reduced = lm(Price ~ Size + Lot, data=Housing) # Reduced model
+> full = lm(Price ~ Size + Lot + Bedrooms + Baths, data=Housing) # Full Model
+> anova(reduced, full) # Compare the models
+Analysis of Variance Table
+Model 1: Price ~ Size + Lot
+Model 2: Price ~ Size + Lot + Bedrooms + Baths
+Res.Df RSS Df Sum of Sq F Pr(>F)
+1 97 9.0756e+10
+2 95 8.5672e+10 2 5083798629 2.8186 0.06469 .
+---
+  Signif. codes: 0 „***‟ 0.001 „**‟ 0.01 „*‟ 0.05 „.‟ 0.1 „ ‟ 1
